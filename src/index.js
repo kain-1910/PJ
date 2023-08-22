4d9fca2192748d24242b51f739db99e95f68eb2e
kain-1910 <pvcong1910@gmail.com>
2023-08-22 23:12:01 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require('path');  // manager paths of app
+const express = require('express');   // express --> develop web based NodeJS
+const app = express();      // return methods of express
+const port = 3000;   // network port
+const morgan = require('morgan');  // morgan --> log HTTP request on terminal
+const handlebars = require('express-handlebars'); // Template engine(handlebars) --> separate the HTML structure 
+app.use(express.static(path.join(__dirname, 'public'))); // config static files(img, video, html, css, ....) from public folder
+app.use(morgan('combined'));  // logger HTTP request using combined format
+
+app.engine('hbs', handlebars.engine({
+  extname:'hbs' // config extension --->(.hbs)
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resouces/views'));
+// define route of request HTTP GET and handle HTTP GET requests
+app.get('/', (req, res) => {  // req-request, res-response
+  res.render('home'); 
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+// listen connections from HTTP requests to the server
+app.listen(port, () => {
+  console.log(`Example app listening on port localhost:${port}`);
+})
+
+
