b6b10b49650bb84bb78bf6e785d9c542e0bc3b67
kain-1910 <pvcong1910@gmail.com>
2023-09-05 22:48:46 +0700

    count deleted documents

--- src/app/controllers/MeControllers.js
+++ src/app/controllers/MeControllers.js
@@ -2,11 +2,21 @@ const Course = require('../models/Course');
 class MeControllers {
     // Lấy dữ liệu từ model
     storedCourses(req, res, next) {
-        Course.find({}).lean()
-            .then(courses => {
-                res.render('me/store_courses', { courses });
+
+        Promise.all([Course.find({}).lean(), Course.countWithDeleted({ deleted:true }).lean()])
+            .then(([courses, deleteCount]) => {
+                res.render('me/store_courses', { 
+                    courses,
+                    deleteCount
+                });
             })
             .catch(next)
+
+        // Course.find({}).lean()
+        //     .then(courses => {
+        //         res.render('me/store_courses', { courses, dl });
+        //     })
+        //     .catch(next)
     }
     trashCourses(req, res, next) {
         Course.findWithDeleted({deleted:true}).lean()
